2feffd253d6634a22ffbc474215d3d5b38ca0b20
Michael J. MacMullin <mike.macmullin@gmail.com>
2023-07-11 17:47:35 -0230

    Update search-engine-configs.ts

--- src/content-script/search-engine-configs.ts
+++ src/content-script/search-engine-configs.ts
@@ -69,7 +69,7 @@ export const config: Record<string, SearchEngine> = {
   },
   searx: {
     inputQuery: ["input[name='q']"],
-    sidebarContainerQuery: ['#sidebar_results'],
+    sidebarContainerQuery: ['#sidebar_results,#sidebar'],
     appendContainerQuery: [],
   },
 }
