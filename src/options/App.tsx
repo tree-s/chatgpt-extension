8fdfaed206b6fa1f79f0ef5c930783362dbbe664
Michael J. MacMullin <mike.macmullin@gmail.com>
2023-07-11 19:59:16 -0230

    Update App.tsx

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -60,21 +60,8 @@ function OptionsPage(props: { theme: Theme; onThemeChange: (theme: Theme) => voi
           <span className="font-semibold">ChatGPT for Search (v{getExtensionVersion()})</span>
         </div>
         <div className="flex flex-row gap-3">
-          <a href="https://chatgpt-for-google.canny.io/changelog" target="_blank" rel="noreferrer">
-            Changelog
-          </a>
           <a
-            href="https://github.com/wong2/chat-gpt-google-extension/issues"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Feedback
-          </a>
-          <a href="https://twitter.com/chatgpt4google" target="_blank" rel="noreferrer">
-            Twitter
-          </a>
-          <a
-            href="https://github.com/wong2/chat-gpt-google-extension"
+            href="https://github.com/tree-s/chatgpt-extension"
             target="_blank"
             rel="noreferrer"
           >
